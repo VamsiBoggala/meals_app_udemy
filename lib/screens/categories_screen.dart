8c22405eb88e69f37cbe2cda5d5ffc874a1e1b97
VamsiBoggala <vamsiboggala@gmail.com>
2022-02-26 11:20:55 +0530

    displaying recipe Items

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,36 @@
+import 'package:example/widgets/category_item.dart';
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+        elevation: 0.0,
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                color: catData.color,
+                title: catData.title,
+                id: catData.id,
+              ),
+            )
+            .toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
