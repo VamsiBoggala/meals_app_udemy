96178042926176206b0e20fd7151374fd6414a4a
VamsiBoggala <vamsiboggala@gmail.com>
2022-04-27 09:35:21 +0530

    complete meal item

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -7,47 +7,117 @@ class MealDetailScreen extends StatelessWidget {
 
   const MealDetailScreen({Key? key}) : super(key: key);
 
+  buildSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.caption,
+      ),
+    );
+  }
+
+  buildContainer( Widget child) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      width: 300,
+      height: 150,
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context)!.settings.arguments as String;
     final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
+    print(selectedMeal.steps);
+    
     return Scaffold(
       appBar: AppBar(
-        title: Text(mealId),
+        title: Text(selectedMeal.title),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: Image.network(
-              selectedMeal.imageUrl,
-              fit: BoxFit.cover,
-              loadingBuilder: (BuildContext context, Widget child,
-                  ImageChunkEvent? loadingProgress) {
-                if (loadingProgress == null) {
-                  return child;
-                } else {
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
+                loadingBuilder: (BuildContext context, Widget child,
+                    ImageChunkEvent? loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child;
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+                errorBuilder: (e, r, s) {
                   return const Center(
                     child: CircularProgressIndicator(),
                   );
-                }
-              },
-              errorBuilder: (e, r, s) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              },
+                },
+              ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 10),
-            child: Text(
-              'Ingredients',
-              style: Theme.of(context).textTheme.caption,
+            buildSectionTitle(context, 'Ingredients'),
+            buildContainer(
+               ListView.builder(
+                itemCount: selectedMeal.ingredients.length, 
+                itemBuilder: (context, index) {
+                  print(selectedMeal.ingredients);
+                  return Card(
+                    color: Theme.of(context).colorScheme.secondary,
+                    child: Container(
+                      margin: const EdgeInsets.all(4.0),
+                      child: Text(
+                        selectedMeal.ingredients[index],
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
-          )
-        ],
+            // Container(
+            //   height: 200,
+            //   width: 300,
+            //   child: ListView.builder(
+            //     itemCount: selectedMeal.ingredients.length,
+            //     itemBuilder: (context, index){
+            //       return Card(
+            //         color: Theme.of(context).accentColor,
+            //         child: Text(selectedMeal.ingredients[index]),
+            //       );
+            //     },
+            //   ),
+            // ),
+            buildSectionTitle(context, 'Steps'),
+            buildContainer(
+               ListView.builder(
+                itemCount: selectedMeal.steps.length,
+                itemBuilder: (context, index) {
+                  
+                  return ListTile(
+                    leading: CircleAvatar(
+                      child: Text('# ${(index + 1)}'),
+                    ),
+                    title: Text(
+                      selectedMeal.steps[index],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
