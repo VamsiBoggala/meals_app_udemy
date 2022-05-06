c6366433de650f67c54a5e76868e6ec7cf1954cd
VamsiBoggala <vamsiboggala@gmail.com>
2022-05-06 22:20:48 +0530

    Passing data back complted

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,14 +1,23 @@
-
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
+import '../widgets/main_drawer.dart';
+
 class FiltersScreen extends StatelessWidget {
-  const FiltersScreen({ Key? key }) : super(key: key);
+  const FiltersScreen({Key? key}) : super(key: key);
   static const routName = '/filters';
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Filter Screen'),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Filters'),
+      ),
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text('Filter Screen'),
+      ),
+    );
   }
 }
